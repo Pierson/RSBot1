17891ff6f0d17caace7917102dac628d2f6ccaa0
Pierson <pierson_barwell@sympatico.ca>
2013-10-30 22:08:59 -0400

    Huge upload

---
+++ Smelter/src/data/Bar.java
@@ -0,0 +1,53 @@
+package data;
+
+public enum Bar {
+    NONE(-1, -1, -1, -1, -1, -1),
+    BRONZE(2349, 436, 438, 14, 14, 14),
+    IRON(2351, 440, 440, 28, 28, 28),
+    STEEL(2353, 440, 453, 9, 19, 9),
+    SILVER(2355, 442, 442, 28, 28, 28),
+    GOLD(2357, 444, 444, 28, 28, 28),
+    MITHRIL(2359, 447, 453, 5, 23, 5),
+    ADAMANT(2361, 449, 453, 4, 24, 4),
+    RUNE(2363, 451, 453, 3, 25, 3);
+    
+    private int barId;
+    private int ingredient1;
+    private int ingredient2;
+    private int withdrawAmt;
+    private int withdrawAmt2;
+    private int maxBar;
+    
+    Bar(final int barId, final int ingredient1, final int ingredient2, final int withdrawAmt, final int withdrawAmt2, final int maxBar) {
+	this.barId = barId;
+	this.ingredient1 = ingredient1;
+	this.ingredient2 = ingredient2;
+	this.withdrawAmt = withdrawAmt;
+	this.withdrawAmt2 = withdrawAmt2;
+	this.maxBar = maxBar;
+    }
+    
+    public int getBar() {
+	return barId;
+    }
+    
+    public int getFirstIngredient() {
+	return ingredient1;
+    }
+    
+    public int getSecondIngredient() {
+	return ingredient2;
+    }
+    
+    public int getWithdrawAmt() {
+	return withdrawAmt;
+    }
+    
+   public int getSecondWithdrawAmt() {
+       return withdrawAmt2;
+   }
+   
+   public int getMaxBar() {
+       return maxBar;
+   }
+}
---
+++ Smelter/src/ui/Gui.java
@@ -0,0 +1,91 @@
+package ui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+
+import pSmelter.*;
+
+import data.Bar;
+import data.Location;
+
+public class Gui {
+    
+    public static boolean started = false;
+
+public Gui() {
+    components();
+}
+
+private void components() {
+    Border raised;
+    TitledBorder title;
+    
+	final JFrame main = new JFrame();
+	main.setLayout(new FlowLayout());
+	main.setSize(220, 200);
+	main.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	main.setResizable(false);
+	
+	final JLabel label = new JLabel("Choose Bar Type:");
+	final JLabel location = new JLabel("Choose Location: (Edge untested)");
+
+	final JComboBox<Bar> scroll = new JComboBox<Bar>(new DefaultComboBoxModel<>(Bar.values()));
+	scroll.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent ae) {
+		Smelter.selected = scroll.getSelectedItem().toString();
+		Smelter.bar = (Bar) scroll.getSelectedItem();
+	    }
+	});
+	
+	final JComboBox<Location> scroll2 = new JComboBox<Location>(new DefaultComboBoxModel<>(Location.values()));
+	scroll2.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent ae) {
+		Smelter.location = (Location) scroll2.getSelectedItem();
+		Smelter.selectedLocation = scroll2.getSelectedItem().toString();
+	    }
+	});
+	
+	final JCheckBox coalbag = new JCheckBox("Use Coalbag");
+	coalbag.setEnabled(false);
+	final JLabel need = new JLabel("Need account to test Coalbag + Edgeville. PM ME");
+	
+	JLabel lab = new JLabel("Your Elite AIO Experience");
+	lab.setFont(new Font("Serif", Font.BOLD, 14));
+	raised = BorderFactory.createRaisedBevelBorder();
+	title = BorderFactory.createTitledBorder(raised, "pSmelter");
+	title.setTitlePosition(TitledBorder.CENTER);
+	lab.setBorder(title);
+	
+	JButton start = new JButton("Start");
+	start.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent ae) {
+		main.dispose();
+		started = true;
+	    }
+	});
+	
+	main.add(lab);
+	main.add(label);
+	main.add(scroll);
+	main.add(location);
+	main.add(coalbag);
+	main.add(scroll2);
+	main.add(start);
+	main.add(need);
+	main.add(main);
+	
+	main.setVisible(true);
+}	
+
+    public static void main(String args[]) {
+	SwingUtilities.invokeLater(new Runnable() {
+	    public void run() {
+		new Gui();
+	    }
+	});
+    }
+}
