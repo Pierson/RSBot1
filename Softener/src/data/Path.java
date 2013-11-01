1c2a58aa7a85f99d1e3d76c1e0d298dcc797cb9a
Pierson <pierson_barwell@sympatico.ca>
2013-10-31 23:22:19 -0400

    Softener push

---
+++ Softener/src/data/Path.java
@@ -0,0 +1,20 @@
+package data;
+
+import org.powerbot.script.wrappers.Tile;
+
+public enum Path {
+    NORTH_WEST(new Tile(3148, 3501, 0)),
+    NORTH_EAST(new Tile(3180, 3501, 0)),
+    SOUTH_WEST(new Tile(3149, 3482, 0)),
+    SOUTH_EAST(new Tile(3180, 3482, 0));
+    
+    private final Tile tile;
+    
+    Path(final Tile tile) {
+	this.tile = tile;
+    }
+    
+    public Tile getPath() {
+	return tile;
+    }
+}
